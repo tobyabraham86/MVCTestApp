fca2f03ecfe97adb8a1adf31cae129c3f2ffe6d6
Toby - Work <Toby-Work@Tobys-MacBook-Pro.local>
2024-12-14 16:40:47 -0500

    Added new About Us Page, Updated Home

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVCTestApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Jokes.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTestApp.Models
+{
+
+    public class Joke
+    {
+        public int ID { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+            JokeAnswer=" ";
+            JokeQuestion= " ";
+        }
+
+    }
+
+}
